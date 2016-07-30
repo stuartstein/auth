5d7d7b15e2e3cc28752014a9cca38cd5158217f5
stuartstein <stuart.stein@gmail.com>
2016-07-29 21:17:16 -0700

    Completed tutorial to setup auth server

---
+++ server/controllers/auth.js
@@ -0,0 +1,52 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({
+        error: 'Please provide both an email and password.'
+      });
+  }
+
+  // See if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    // if a user with a email does exist, return an Error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+      res.json({ token: tokenForUser(user) });
+    });
+  });
+
+
+
+
+
+  // Respond to request indicating the user was created
+}
